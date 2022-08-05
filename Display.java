a3805653dcfa4bec2747676f34068d1d8280f41b
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2022-08-06 00:41:32 +0700

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,5 +1,6 @@
 public class Display {
     public static void main(String[] args) {
+        //Beakkakakkakakmak
         System.out.print("Welcome to");
         System.out.println("BEAM1234");
     }
